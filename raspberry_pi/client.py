4b40300af53af8a51a8e08516e3cf46bc9500da3
jay <jhanmtl@gmail.com>
2019-09-13 17:44:44 -0400

    Create client.py

---
+++ raspberry_pi/client.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 17:22:53 2019
+
+@author: jh
+"""
+
+import utils
+import time
+import cv2
+import argparse
+
+ap=argparse.ArgumentParser()
+ap.add_argument('img',type=str)
+ap.add_argument('data',type=str)
+args=ap.parse_args()
+data_url=args.data
+img_url=args.img
+
+data_cap=utils.frameInfoGrabber(data_url).start()
+img_cap=utils.frameGrabber(img_url).start()
+time.sleep(5)
+
+while True:
+    info=data_cap.read()
+    img=img_cap.read()
+    if info!='pass':
+        fps,delta_x,delta_y,label=utils.draw(info,img)
+    cv2.imshow('',img)
+
+    if cv2.waitKey(1)==ord('q'):
+        cv2.destroyAllWindows()
+        break
+
+        
